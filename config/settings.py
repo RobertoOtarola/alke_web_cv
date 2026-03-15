20fca8cbb9f53b5aa0084f2a7f33975bb3d892cf
Roberto Otarola <roberto.otarola@proton.me>
2026-03-15 16:24:24 -0300

    chore(config): externalize SECRET_KEY and DEBUG via python-decouple

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cv_editor",
 ]
 
 MIDDLEWARE = [
