a579853362c381e2c335e9a51b62153ea081cd1b
Roberto Otarola <roberto.otarola@proton.me>
2026-04-02 12:35:50 -0300

    fix: make cv.0006 migration resilient and restore standard build process

--- build.sh
+++ build.sh
@@ -10,8 +10,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Aplicar migraciones
-# NOTA: cv:0006 da error en Supabase porque los campos ya existen. Se marca como falsa.
-python manage.py migrate cv 0006 --fake --no-input
-
-# Aplicar el resto de las migraciones
 python manage.py migrate --no-input
