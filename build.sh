ca171b0cff284b0e284040de451c984574bd7a3e
Roberto Otarola <roberto.otarola@proton.me>
2026-04-02 11:34:25 -0300

    fix: resolve Render build failure by faking cv.0006 migration

--- build.sh
+++ build.sh
@@ -10,4 +10,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Aplicar migraciones
-python manage.py migrate
+# NOTA: cv:0006 da error en Supabase porque los campos ya existen. Se marca como falsa.
+python manage.py migrate cv 0006 --fake --no-input
+
+# Aplicar el resto de las migraciones
+python manage.py migrate --no-input
